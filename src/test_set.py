4249f5ec1bd6f11f1e92b52ee304fb86628064d1
kusah <kunalsah3108@gmail.com>
2026-06-24 19:54:45 +0530

    Initial project structure

---
+++ src/test_set.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import xgboost
+import shap
+
+print("Pandas:", pd.__version__)
+print("NumPy:", np.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("XGBoost:", xgboost.__version__)
+print("SHAP:", shap.__version__)
+
+print("\nSetup Successful!")
